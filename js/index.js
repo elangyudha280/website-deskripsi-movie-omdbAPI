bbd6606544ecbcdaf35026887f2a7af30e6375f7
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-07-26 18:53:49 +0700

    selesai buat function manggil API

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 
 
 
+
+
+// buat fitur api data movie
+
+// 1.buat function untk manggil data api
+
+function dataMovie(keyword){
+    return fetch(`http://www.omdbapi.com/?apikey=1a8df092&s=${keyword}`)
+    .then((data_api) => data_api.json())
+    .then((respone) => {
+        console.log(respone)
+    })
+}
+
+
+// 2.buat event untuk ambil datanya dari inputan modal search
+// variable element button seacrhnya
